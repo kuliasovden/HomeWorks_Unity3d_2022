3f1eec6f7d81e4ad86583b3d6b45ecd2dd4385ab
KuliasovD <taviskaron99@gmail.com>
2022-02-16 16:44:47 +0200

    Перезалито

---
+++ Homework №1/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Vector3 camerPposition = new Vector3(0f, 2f, -4f);
+
+    private Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.Find("Player").transform;        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = target.TransformPoint(camerPposition);
+        transform.LookAt(target);
+    }
+}
---
+++ Homework №1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float rotateSpeed;
+    [SerializeField] private float jumpVelocity;
+
+    private float verticalInput;
+    private float horizontalInput;
+    private bool isOnGround = true;
+    private Rigidbody _playerRb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _playerRb = GetComponent<Rigidbody>();
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Control();
+        Jump();
+    }
+    private void Control()
+    {
+        verticalInput = Input.GetAxis("Vertical") * moveSpeed;
+        horizontalInput = Input.GetAxis("Horizontal") * rotateSpeed;
+
+
+        Vector3 rotation = Vector3.up * horizontalInput;
+        Quaternion angleRotation = Quaternion.Euler(rotation * Time.fixedDeltaTime);
+
+        _playerRb.MovePosition(transform.position + transform.forward * verticalInput * Time.fixedDeltaTime);
+        _playerRb.MoveRotation(_playerRb.rotation * angleRotation);
+
+    }
+
+    private void Jump()
+    {
+        if (isOnGround && Input.GetKeyDown(KeyCode.Space))
+        {
+            _playerRb.AddForce(Vector3.up * jumpVelocity, ForceMode.Impulse);
+            isOnGround = false;
+        }
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
+    }
+
+}
